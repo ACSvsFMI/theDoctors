55c18614dfadb087a87396816625f94819653f2e
Cruceru Radu <radu.cruceru@gmail.com>
2012-12-08 20:40:08 +0200

    retrieved suggested people

--- trakc/frontend/static/js/controllers/manage.js
+++ trakc/frontend/static/js/controllers/manage.js
@@ -3,5 +3,20 @@ function ManageController($scope, $http) {
     $http.get('api/v1/target/').success(function(data) {
         $scope.targets = data.objects;
     })
+
+    $scope.fetch_partial_results = function() {
+        var t_name = $scope.target_name
+   
+        if (typeof t_name == 'undefined') {
+            return;     
+        }
+
+        $http.get('suggestion?name=' + t_name).success(
+            function (data) {
+                $scope.suggestions = data
+            });
+ 
+    }
+
 }
 
