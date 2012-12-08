0f22146a9869965b0d3f695b6933b410d2e88250
Cruceru Radu <radu.cruceru@gmail.com>
2012-12-08 14:34:40 +0200

    fixed crap in app

--- trakc/trakc/models/__init__.py
+++ trakc/trakc/models/__init__.py
@@ -1,7 +1,4 @@
-from target import Target
-from target import UsersAndTargets
-from post import Post
-from post import ActionsAndPosts
-from action import Action
+from target import Target, UsersAndTargets
+from post import Post, ActionsAndPosts
 from action import UsersAndActions
 
--- trakc/trakc/models/action.py
+++ trakc/trakc/models/action.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from json_field import JSONField
 
 class Action(models.Model):
     
@@ -9,11 +8,15 @@ class Action(models.Model):
 
     name = models.CharField(max_length=16)
     
-    cost = models.ManyToMany(User, through=UsersAndActions)
+    cost = models.ManyToManyField(User, through='UsersAndActions')
 
 class UsersAndActions(models.Model):
     
+    class Meta:
+        app_label = 'trakc'
+
     user = models.ForeignKey(User)
     action = models.ForeignKey(Action)
 
     action_cost = models.IntegerField(default=1)
+
